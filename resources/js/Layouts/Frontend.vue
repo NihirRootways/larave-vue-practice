4f4b85ceae5d108a49f563af90a53d1aa6282f1d
Nihir R <112462241+mec0dy@users.noreply.github.com>
2022-11-01 10:09:26 +0530

    Adding the header

---
+++ resources/js/Layouts/Frontend.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HeaderVue from '@/Components/Frontend/Header.vue';
+</script>
+<template>
+    <div class="bg-slate-200 dark:bg-slate-900">
+        <!-- Header -->
+        <HeaderVue />
+        <main class="min-h-screen">
+            <slot/>
+        </main>
+        <!-- Footer -->
+    </div>
+</template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,14 +1,9 @@
 <script setup>
 import { Head, Link } from '@inertiajs/inertia-vue3';
-
-defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
-})
+import FrontendVue from '@/Layouts/Frontend.vue';
 </script>
 
 <template>
     <Head title="Welcome to portfolio" />
+    <FrontendVue />
 </template>
