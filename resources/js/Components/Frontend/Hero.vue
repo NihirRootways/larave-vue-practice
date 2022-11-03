731dba662cbc9cd6251e0a4e2c92f1150ac95b9c
Nihir R <112462241+mec0dy@users.noreply.github.com>
2022-11-03 17:24:00 +0530

    Hero Mode

--- resources/js/Components/Frontend/Hero.vue
+++ resources/js/Components/Frontend/Hero.vue
@@ -28,7 +28,7 @@
     >
       <div class="flex flex-col md:flex-row items-center h-full pt-8">
         <div class="flex-1 flex flex-col items-center lg:items-start">
-          <p class="text-lg text-accent text-md mb-[22px]">Hey, I'm Tony! 👋</p>
+          <p class="text-lg text-accent text-md mb-[22px]">Hey, I'm Nihir! 👋</p>
           <h1
             class="
               text-4xl
